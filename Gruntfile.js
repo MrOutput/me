3ba0f0f15d56ab0c4813f082b3a60f2a4687cb8b
Rafael Cepeda <rcepeda1993@gmail.com>
2015-07-03 00:52:09 -0400

    added grunt dev automation and mobile device specific html/css

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jade: {
+      files: {
+        src: 'index.jade',
+        dest: 'index.html'
+      }
+    },
+    less: {
+      files: {
+        src: 'css/layout.less',
+        dest: 'css/layout.css'
+      }
+    },
+    watch: {
+      jade: {
+        files: 'index.jade',
+        tasks: 'jade',
+        options: {
+          event: 'changed'
+        }
+      },
+      less: {
+        files: 'css/layout.less',
+        tasks: 'less',
+        options: {
+          event: 'changed'
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['watch']);
+
+};
